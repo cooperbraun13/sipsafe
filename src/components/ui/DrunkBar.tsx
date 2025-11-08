ec773f6bdb1f1cfe38e2d57a673729d573277f4f
ADH-17 <ayden.humphries@gmail.com>
2025-11-08 15:42:48 -0800

    feat[colors] COLOR GRADIENT!!!!! PRETTY!!!!

--- src/components/ui/DrunkBar.tsx
+++ src/components/ui/DrunkBar.tsx
@@ -1,13 +1,17 @@
-export default function DrunkBar(){
-    return (
-        <>
-        <div className="grid justify-center p-[4vh]">
-            <div className="flex flex-row w-[75vw] h-[6vh]">
-                <div className="flex-1 bg-[#22bb45]"></div>
-                <div className="flex-1 bg-[#FFFF00]"></div>
-                <div className="flex-1 bg-[#C41E3A]"></div>
-            </div>
+export default function DrunkBar() {
+  return (
+    <div className="flex justify-center items-center h-[15vh]">
+      <div className="flex flex-row w-[75vw] h-[6vh]">
+        <div className="flex-1 bg-[#22bb45] flex justify-center items-center text-3xl rounded-tl-md rounded-bl-md">
+          😊
         </div>
-        </>
-    )
+        <div className="flex-1 bg-gradient-to-r from-[#22bb45] via-[#FFFF00] to-[#C41E3A] flex justify-center items-center text-3xl ">
+          🥴
+        </div>
+        <div className="flex-1 bg-[#C41E3A] flex justify-center items-center text-3xl rounded-tr-md rounded-br-md">
+          🤢
+        </div>
+      </div>
+    </div>
+  );
 }
